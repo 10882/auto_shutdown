37776e3cf7ddbffe6d973ee7d4e7c572af47e3e3
10882 <62587713+10882@users.noreply.github.com>
2021-06-16 23:49:29 +0300

    added program

---
+++ auto_shotdown/auto_shotdown/auto_shotdown.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	int input;
+
+	cout << "enter active time (minutes) \n";
+	cin >> input;
+	
+	
+	input = input * 1000 * 60;
+
+	Sleep(input);
+
+	system("shutdown /s");
+	cout << "You PC will shutdown soon";
+}
